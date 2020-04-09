312566a08de7ff15b8e86de6267a8e981a175cb7
thangtienql <thangtienql@gmail.com>
2020-04-09 10:57:25 +0700

    update code

---
+++ js/main/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // set an element
+    $("#date").val( moment().format('D MMM, YYYY') );
+
+    // set a variable
+    var today = moment().format('D MMM, YYYY');
+
+});
